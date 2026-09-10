9f71df6ba26c375b3370ceadfe179999acd5e837
Shubham Jagtap <shubham.trinity246@gmail.com>
2026-09-10 12:51:20 +0530

    Fix ReactNode import build error

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -1,4 +1,3 @@
-import type { ReactNode } from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
 import { useLocation, Outlet } from 'react-router-dom';
 import GlobalSidebar from './GlobalSidebar';
